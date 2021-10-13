6ebb2406aa56dce077e10af33dfe3f818d4c5aae
EvertonHimself <evertondeoliveira@gmail.com>
2021-10-12 21:34:56 -0300

    Add a char counter to the code.
    Added some lines of code to count how many characters a given hidden word have.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,11 +4,36 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to Bull-Cow Game!");
-    PrintLine("Press Enter to Continue...");
+    
+    SetupGame();
+
+    //PrintLine(TEXT("The HiddenWord is: %s.\nIt is %i characters long"), *HiddenWord, HiddenWord.Len()); // Debug line.
+
+    PrintLine(TEXT("Welcome to Bull-Cow Game!"));
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess and\npress enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You have won!"));
+    }
+    else
+    {
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The hidden word is %i characters long, try again!"), HiddenWord.Len());
+        }
+        PrintLine(TEXT("You have lost!"));
+    }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("pages");
+    Lives = 4;
 }
