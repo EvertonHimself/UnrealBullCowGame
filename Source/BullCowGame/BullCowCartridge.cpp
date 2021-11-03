486da5b623134d169e008065519982a9db9d69c3
EvertonHimself <evertondeoliveira@gmail.com>
2021-11-02 21:21:35 -0300

    Cleaned comments.
    Cleaned comments. I've removed a bunch of comments to make the code clearer.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
-//#include "HiddenWordList.h";
 
-void UBullCowCartridge::BeginPlay() // When the game starts
+void UBullCowCartridge::BeginPlay() // When the game starts.
 {
     Super::BeginPlay();
     
@@ -10,43 +9,28 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 
     const FString WordListPath = FPaths::ProjectContentDir() / TEXT("WordLists/HiddenWordList.txt");
     FFileHelper::LoadFileToStringArray(Words, *WordListPath);
-    // PrintLine(TEXT("The number of possible words is %i"), Words.Num());
-    // PrintLine(TEXT("The first 5 valid words are:"));
+
     GetValidWords(Words);
 
-    // PrintLine(TEXT("The number of valid words is: %i"), GetValidWords(Words).Num());
-    // PrintLine(TEXT("ValidWord - 1 is = %i"), GetValidWords(Words).Num() - 1);
     SetupGame();
-
 }
 
 TArray <FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordsList) const
 {
     TArray<FString> ValidWords;
 
-    // Former "for" parameters: int32 i = 0; i < WordsList.Num(); i++.
     for (FString Word : WordsList)
     {
         if (Word.Len() >= 4 && Word.Len() <= 8 && IsIsogram(Word))
         {
             ValidWords.Emplace(Word);
-        
-            // I don't know WHY it only works with the "*"" operator (*Words).
-            // The * is called "dereference", and i have no idea why and what it is for.
-            //PrintLine(TEXT("%s"), *Words[i]);
         }
     }
-
-    // for (int32 i = 0; i < ValidWords.Num(); i++)
-    // {
-    //     PrintLine(TEXT("%s."), *ValidWords[i]);
-    // }
 
     return ValidWords;
 }
 
-// What's the difference between & and *? (Kinda answered on 77. Introduction To References)
-void UBullCowCartridge::OnInput(const FString& PlayerInput) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& PlayerInput) // When the player hits enter.
 {
     if (bGameOver == true)
     {
@@ -68,19 +52,12 @@ void UBullCowCartridge::SetupGame()
     bGameOver = false;
     Lives = HiddenWord.Len();
 
-    // What's the difference between the dot operator and the :: operator?
     PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
     PrintLine(TEXT("You have %i lives left."), Lives);
     PrintLine(TEXT("Type in your guess and\npress enter to continue..."));
 
-    //const TCHAR HW[] = TEXT("pages");
-    //const TCHAR HW[] = {TEXT('p'), TEXT('a'), TEXT('g'), TEXT('e'), TEXT('s'), TEXT('\0')};
-    //HW;
-    //PrintLine(TEXT("Character 1 of the hidden word is: %c"), HiddenWord[0]);
-    //PrintLine(TEXT("The 4th character of HW is: %c"), HW[3]); // Should print 'e'.
-    // I've forgot why we have to use the *HiddenWord...
     PrintLine(TEXT("The HiddenWord is: %s.\nIt is %i characters long"), *HiddenWord, HiddenWord.Len()); // Debug line.
-    
+
     IsIsogram(HiddenWord);
 }
 
@@ -109,14 +86,11 @@ void UBullCowCartridge::ProcessGuess(const FString& Guess)
 
     if (!IsIsogram(Guess))
     {
-        /* code */
         PrintLine(TEXT("No repeating letters, guess again"));
         return;
     }
 
     PrintLine(TEXT("Lost a life!"));
-    // Using Lives-- will show the value of Lives BEFORE it was decremented. Ex: It will show
-    // 5 even though the actual value is 4.
     PrintLine(TEXT("%i"), --Lives);
 
     if (Lives <= 0)
@@ -147,18 +121,4 @@ bool UBullCowCartridge::IsIsogram(const FString& Word) const
     }
     
     return true;
-
-    // int32 Index = 0;
-    // for (Comparison = Index + 1; Comparison < Word.Len(); Comparison++)
-    // {
-    //     if (Word[Index] == Word[Comparison])
-    //     {
-    //         return false;
-    //     }
-    // }
-
-    // for (int32 Index = 0; Index < Word.Len(); Index++)
-    // {
-    //     PrintLine(TEXT("%c"), Word[Index]);
-    // }
 }
